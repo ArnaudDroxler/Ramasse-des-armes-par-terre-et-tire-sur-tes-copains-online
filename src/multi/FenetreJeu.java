5fefc46ae9f8daa314968394a1bd1514708c6a9d
joaquim <joaquim.perez@he-arc.ch>
2016-08-22 15:19:52 +0200

    touche escape pour sortir du jeu

--- src/multi/FenetreJeu.java
+++ src/multi/FenetreJeu.java
@@ -4,6 +4,8 @@ import java.awt.AWTException;
 import java.awt.Cursor;
 import java.awt.Point;
 import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
@@ -15,6 +17,7 @@ public class FenetreJeu extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
 	private Robot robot;
+	private boolean robotOff;
 	
 	public FenetreJeu(String pngFileName) throws AWTException {
 		Logique logique = new Logique(pngFileName);
@@ -37,14 +40,17 @@ public class FenetreJeu extends JFrame {
 		setVisible(true);
 
 		robot = new Robot();
+		robotOff=false;
 		
 		addMouseMotionListener(new MouseMotionAdapter() {
 
 			@Override
 			public void mouseMoved(MouseEvent e) {
-				int dx = e.getX() - getWidth()/2;
-				logique.heros.rotate(dx);
-				robot.mouseMove((int) (getLocation().getX()+getWidth()/2), (int) (getLocation().getY()+getHeight()/2));
+				if(!robotOff){
+					int dx = e.getX() - getWidth()/2;
+					logique.heros.rotate(dx);
+					robot.mouseMove((int) (getLocation().getX()+getWidth()/2), (int) (getLocation().getY()+getHeight()/2));
+				}
 			}
 		});
 		
@@ -52,8 +58,10 @@ public class FenetreJeu extends JFrame {
 			
 			@Override
 			public void mouseEntered(MouseEvent e) {
+				robot.mouseMove((int) (getLocation().getX()+getWidth()/2), (int) (getLocation().getY()+getHeight()/2));
 				// cache le curseur
 				setCursor(getToolkit().createCustomCursor( new BufferedImage(3, 3, BufferedImage.TYPE_INT_ARGB), new Point(0, 0),"null"));
+				robotOff=false;
 			}
 
 			@Override
@@ -61,6 +69,29 @@ public class FenetreJeu extends JFrame {
 				logique.fire();
 			}
 			
+		});
+		
+		addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
+					 robotOff = true;
+					 setCursor(Cursor.getDefaultCursor());
+				}
+			}
 		});
 
 	}
