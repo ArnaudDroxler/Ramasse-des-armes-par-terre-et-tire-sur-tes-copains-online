411665d31c61e6d50a4f0f4613ae5e2e2f345580
joaquim <joaquim.perez@he-arc.ch>
2016-08-30 09:30:38 +0200

    version simplifiée du dernier commit

--- src/tests/kryonet/implem/logiqueCoteClient/MainServer.java
+++ src/tests/kryonet/implem/logiqueCoteClient/MainServer.java
@@ -5,11 +5,7 @@ import tests.kryonet.implem.logiqueCoteClient.server.PcServer;
 public class MainServer {
 
 	public static void main(String[] args) {
-		if(args.length>0){
-			new PcServer(true);
-		}else{
-			new PcServer(false);
-		}
+		new PcServer(args.length>0);
 	}
 
 }
